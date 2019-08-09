0af8163f8a2fdf9bb64c4b52cb7ea3d68ac82f50
Chrissy Hunt <chrissy.hunt@gmail.com>
2019-08-08 20:58:17 -0400

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import db from './firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ingredients: [],
+  }
+
+  componentDidMount = () => {
+    db.collection('ingredients').onSnapshot(snapshot => {
+      let ingredients = snapshot.docs;
+      let newState = [];
+      for (let item in ingredients) {
+        newState.push(ingredients[item].data());
+      }
+      this.setState({
+        ingredients: newState,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Current Ingredients</h1>
+        <ul>
+        { this.state.ingredients.map(item => (
+          <li key={item.label}>{item.measurement} {item.unit} {item.label}</li>
+        ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
