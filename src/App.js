dc2f370075f9798c3c5e59541be2cd4f0cb70110
Chrissy Hunt <chrissy.hunt@gmail.com>
2019-08-31 07:21:50 -0400

    Add, connect react-swipeable-views

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { bindKeyboard } from 'react-swipeable-views-utils';
 import AddRecipe from './components/AddRecipe';
 import AddIngredient from './components/AddIngredient';
 import Ingredients from './components/Ingredients';
 import Recipes from './components/Recipes';
+import Home from './components/Home';
+
+const BindKeyboardSwipeableViews = bindKeyboard(SwipeableViews);
 
 class App extends Component {
   state = {
@@ -12,12 +17,40 @@ class App extends Component {
   render() {
     return (
       <>
-      <Ingredients setTargetIngredient={this.setTargetIngredient} />
-      <Recipes />
-      <AddIngredient ingredientToEdit={this.state.targetIngredient} resetEditIngredient={this.resetEditIngredient} />
-      <AddRecipe />
+      <BindKeyboardSwipeableViews enableMouseEvents>
+        <div key={0} style={{...styles.slide, ...styles.slide0}}>
+          <Home />
+        </div>
+        <div key={1} style={{...styles.slide, ...styles.slide1}}>
+          <Ingredients setTargetIngredient={this.setTargetIngredient} />
+        </div>
+        <div key={2} style={{...styles.slide, ...styles.slide2}}>
+          <Recipes />
+        </div>
+        <div key={3} style={{...styles.slide, ...styles.slide3}}>
+          <AddIngredient ingredientToEdit={this.state.targetIngredient} resetEditIngredient={this.resetEditIngredient} />
+          <AddRecipe />
+        </div>
+      </BindKeyboardSwipeableViews>
       </>
     );
+  }
+}
+
+const styles = {
+  slide: {
+    padding: '15em',
+    minHeight: '100vh',
+    color: '#FFF',
+  },
+  slide1: {
+    background: 'red',
+  },
+  slide2: {
+    background: 'yellow',
+  },
+  slide3: {
+    background: 'green',
   }
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        <button>View Recipes</button><br />
+        <button>View Ingredients</button><br />
+        <button>Find My Dinner</button>
+      </div>
+    );
+  }
+}
+
+export default Home;
